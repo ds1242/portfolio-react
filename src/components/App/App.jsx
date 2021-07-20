88f30fc012db90661d6af44d204e65721dc5701c
ds1242 <david.shaw1242@gmail.com>
2021-07-20 09:47:28 -0600

    install testing and bootstrap started nav bar

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Nav from '../Nav';
+
+function App() {
+  return (
+   <div className="App">
+      <Nav></Nav>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+    return (
+        <header data-testid="header" className="flex-row px-1">
+            {/* <nav> */}
+                {/* <h2>
+                    <a data-testid="link" href="/">
+                        David's Portfolio
+                    </a>
+                </h2>
+                
+                    <ul className="flex-row">
+                        <li className="mx-2">
+                            <a data-testid="about" href="#about">About Me</a>
+                        </li>
+                        <li>Contact</li>
+                        <li>Portfolio</li>
+                        <li>Resume</li>
+                    </ul> */}
+            {/* </nav> */}
+        </header>
+    )
+};
+
+export default Nav;
