28cfbbf4984e62c5d3a1dcd736a5bf7a1a003d99
ds1242 <david.shaw1242@gmail.com>
2021-07-21 08:58:22 -0600

    added some footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
     return (
-        <footer className="footer" datatest-id="footer">
-            <div className="container-fluid">
-                <div className="row">
-                    <h5 id="info-id">My Info</h5>
-                </div>
-                <div className="row" id="personal-links">
-                    <a href="https://github.com/ds1242" target="_blank"><i class="bi bi-github"></i></a> 
-                    <a href="https://www.linkedin.com/in/david-shaw1242" target="_blank"><i class="bi bi-linkedin"></i></a> 
-                </div>
-                <div className="row">
+        <footer className="footer mt-auto " datatest-id="footer">
+            <div className="list-group list-group-horizontal list-group-flush">
+                {/* <div className="row flex-row"> */}
+                    <h5 id="info-id" className="list-group-item">My Info</h5>
+                {/* </div> */}
+                {/* <div className="row flex-row" id="personal-links"> */}
+                    <a href="https://github.com/ds1242" target="_blank" rel='noreferrer noopener' className="list-group-item"><i className="bi bi-github"></i></a> 
+                    <a href="https://www.linkedin.com/in/david-shaw1242" target="_blank" rel='noreferrer noopener' class="list-group-item"><i className="bi bi-linkedin"></i></a> 
+                    
+                {/* </div> */}
+                {/* <div className="row">
                     &copy; {new Date().getFullYear()} 
-                </div>
+                </div> */}
             </div>
 
         </footer>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+
 // import $ from 'jquery';
 // import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
