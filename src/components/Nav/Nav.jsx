12aefa1403f54ef29b2165d4a4bb3353ec900135
ds1242 <david.shaw1242@gmail.com>
2021-07-21 14:05:35 -0600

    added some media query styling to help styling be more mobile friendly

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,22 +5,32 @@ import './Nav.css';
 
 function Nav() {
     return (
-        <nav className="navbar" data-testid="navbar">
-            <div className="container-fluid navbar-expand-lg">
+        <header>            
+        <nav className="navbar navbar-expand-sm" data-testid="navbar">
+            <div className="container-fluid ">
                 <a className="navbar-brand"  data-testid="link" href="/">
                     <h1>David's Portfolio</h1>
                 </a>
-            
-                <div className="d-flex">
-                    <div className="navbar-nav">
-                        <a className="nav-link active" data-testid="about" href="#about"><h3>About Me</h3></a>
-                        <a className="nav-link active" href="#work"><h3>Work</h3></a>
-                        <a className="nav-link active" href="#contact-me"><h3>Contact</h3></a>
-                        <a className="nav-link active" href="#resume"><h3>Resume</h3></a>
-                    </div>
-                </div>
+                
+                {/* <div className="d-flex mr-lg-2 " id="link-list"> */}
+                    <ul className="navbar-nav ">
+                        <li>
+                            <a className="nav-link active" data-testid="about" href="#about"><h3>About Me</h3></a>
+                        </li>
+                        <li>
+                            <a className="nav-link active" href="#work"><h3>Work</h3></a>                            
+                        </li>
+                        <li>
+                            <a className="nav-link active" href="#contact-me"><h3>Contact</h3></a>
+                        </li>
+                        <li>
+                            <a className="nav-link active" href="#resume"><h3>Resume</h3></a>
+                        </li>
+                    </ul>
+                {/* </div> */}
             </div>
         </nav>
+        </header>
 
     )
 };
