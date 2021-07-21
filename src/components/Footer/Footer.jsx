9509dbb87c837dd46aea0fa63485f9be98513a07
ds1242 <david.shaw1242@gmail.com>
2021-07-21 12:31:48 -0600

    added copyright and date at bottom of footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,11 @@ function Footer() {
     return (
         <footer className="footer mt-auto " datatest-id="footer">
             <div>
-                    <h5 id="info-id">My Info:
-                    <a href="https://github.com/ds1242" target="_blank" rel='noreferrer noopener'><i className="bi bi-github"></i></a> 
-                    <a href="https://www.linkedin.com/in/david-shaw1242" target="_blank" rel='noreferrer noopener'><i className="bi bi-linkedin"></i></a> 
-                    </h5>
+                <h5 id="info-id">My Info:
+                <a href="https://github.com/ds1242" target="_blank" rel='noreferrer noopener'><i className="bi bi-github"></i></a> 
+                <a href="https://www.linkedin.com/in/david-shaw1242" target="_blank" rel='noreferrer noopener'><i className="bi bi-linkedin"></i></a> 
+                </h5>
+                <h5 datatest-id="date">Copyright &copy; {new Date().getFullYear()}</h5>
             </div>
 
         </footer>
