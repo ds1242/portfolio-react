28cf74fe06bdacb5a4f32534ba9a1039efee0905
ds1242 <david.shaw1242@gmail.com>
2021-07-22 07:22:38 -0600

    changed about me text to be a prop passed down to the component to simplify future updates

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ import awesomeImage from '../../assets/imgs/IMG_3529-min.jpg';
 import './About.css';
 
 
-function About() {
+function About(props) {
     return (
         <section id="about-section">            
             <img src={coverImage} className="cover-image" style={{ width: "100%" }} alt="cover" />
@@ -17,9 +17,7 @@ function About() {
                         <img src={awesomeImage} className="personal-photo" id="about-me-img" alt='David hiking in fall' />
                     </div>
                     <div className="col-lg-6" id='about-text'>
-                        <p>
-                            Full stack web developer with several years of Business Analyst experience and a degree in Information Systems from the University of Utah.  Recently earned a certificate in full stack web development from the University of Utah. Skills in HTML, CSS, Javascript, and Node.js.  Always learning new technologies and improving to strive to create impactful web applications for end users.  My variety of previous experiences provide me with a unique perspective to know when and what questions to ask to ensure that applications provide the best results and utility for end users.      
-                        </p>
+                        <p>{props.aboutText}</p>
                     </div>
                 </div>
             </div>    
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,11 +8,12 @@ import Resume from '../Resume';
 
 
 function App() {
+  const aboutText = "Full stack web developer with several years of Business Analyst experience and a degree in Information Systems from the University of Utah.  Recently earned a certificate in full stack web development from the University of Utah. Skills in HTML, CSS, Javascript, and Node.js.  Always learning new technologies and improving to strive to create impactful web applications for end users.  My variety of previous experiences provide me with a unique perspective to know when and what questions to ask to ensure that applications provide the best results and utility for end users.";
   return (
    <div className="App">
       <Nav></Nav>
       <main>
-        <About></About>
+        <About aboutText={aboutText}></About>
 
         <ContactForm></ContactForm>
         <Resume></Resume>
