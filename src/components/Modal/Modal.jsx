ddd6827aec1ee99187cf61e7855a6dbfe6b2d5a4
ds1242 <david.shaw1242@gmail.com>
2021-07-23 12:05:47 -0600

    attempting to get bootstrap modal to load

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -7,19 +7,39 @@ function Modal(props) {
     // console.log('id', id);
     // console.log('description', description);
     return (
-            <div className="modalBackdrop">
-                <div className="modalContainer">
-                    <h3 className="modal-title">{title}</h3>
-                    <img src={require(`../../assets/imgs/${id}.jpg`).default} alt={alt} />
-                    <h4>{description}</h4>
-                    <h4>Built with: {tools1}</h4>
-                    <h4>{tools2}</h4>
-                    <h4>Github:<a href={github}>{github}</a></h4>
-                <button type="button"  onClick={ onClose }>
-                    Close this modal
-                </button>
+            // <div className="modalBackdrop">
+            //     <div className="modalContainer">
+            //         <h3 className="modal-title">{title}</h3>
+            //         <img src={require(`../../assets/imgs/${id}.jpg`).default} alt={alt} />
+            //         <h4>{description}</h4>
+            //         <h4>Built with: {tools1}</h4>
+            //         <h4>{tools2}</h4>
+            //         <h4>Github:<a href={github}>{github}</a></h4>
+            //     <button type="button"  onClick={ onClose }>
+            //         Close this modal
+            //     </button>
+            //     </div>
+            // </div>
+            <div className="modal" >
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Modal title</h5>
+                        {console.log('modal trigger')}
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <p>Modal body text goes here.</p>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
                 </div>
-            </div>
+                </div>
     )
 
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// import $ from 'jquery';
-// import Popper from 'popper.js';
+import $ from 'jquery';
+import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './components/App';
 import "bootstrap-icons/font/bootstrap-icons.css";
