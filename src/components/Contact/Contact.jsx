4a602b24cec1a25d76ab0a2797d3ef12a62f0a8d
ds1242 <david.shaw1242@gmail.com>
2021-07-28 17:08:55 -0600

    added some styling to contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { validateEmail } from '../../assets/utils/helpers';
-
+import './Contact.css';
 
 function ContactForm() {
     const [ formState, setFormState] = useState({ name: '', email: '', message: ''});
@@ -43,26 +43,37 @@ function ContactForm() {
     return (
         <section>
             <h2 datatest-id='contact' id='contact-me'>Contact Me</h2>
-            <form id='contact-form' onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input type='text' name='name' defaultValue={name} onBlur={handleChange} />
+            <div className='row justify-content-md-center'>
+                <div className="col col-8">
+                    <form id='contact-form' onSubmit={handleSubmit}>
+                        <div className="form-row">
+                            <div className="form-group">
+                                <label htmlFor="name">Name:</label>
+                                <input type='text' className="form-control" name='name' defaultValue={name} onBlur={handleChange} />
+                            </div>
+                        </div>
+                        <div className="form-row">
+                            <div className="form-group">
+                                <label htmlFor="email">Email:</label>
+                                <input type='email' className="form-control" name='email' defaultValue={email} onBlur={handleChange} />
+                            </div>
+                        </div>
+                        <div className="form-row">
+                            <div className="form-group">
+                                <label htmlFor="message">Message:</label>
+                                <textarea name='message' className="form-control" rows="5" defaultValue={message} onBlur={handleChange} />
+                            </div>
+                        </div>
+                        {errorMessage && (
+                            <div>
+                                <p className="error-message">{errorMessage}</p>
+                            </div>
+                        )}
+                        <button type="submit" data-testid="submitButton" className='btn btn-primary btn-lg' id='form-button'>Submit</button>
+                    </form>
                 </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type='email' name='email' defaultValue={email} onBlur={handleChange} />
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea name='message' rows="5" defaultValue={message} onBlur={handleChange} />
-                </div>
-                {errorMessage && (
-                    <div>
-                        <p className="error-message">{errorMessage}</p>
-                    </div>
-                )}
-                <button type="submit" data-testid="submitButton" className='btn btn-primary btn-lg'>Submit</button>
-            </form>
+            </div>
+
         </section>
     )
 }
