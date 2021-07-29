af5d4b1c343e61facd9a71af7881ad1fd3ab32ea
ds1242 <david.shaw1242@gmail.com>
2021-07-29 08:01:13 -0600

    added third icon and fixed footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import './Footer.css';
+// import './Footer.css';
 
 function Footer() {
     return (
         <footer className="footer mt-auto" datatest-id="footer">
-            <div>
-                <h5 id="info-id">My Info:
-                <a href="https://bit.ly/3eKK7uy" target="_blank" rel='noreferrer noopener'><i className="bi bi-github"></i></a> 
-                <a href="https://bit.ly/3x0CDd4" target="_blank" rel='noreferrer noopener'><i className="bi bi-linkedin"></i></a> 
-                </h5>
-                <h5 datatest-id="date">Copyright &copy; {new Date().getFullYear()}</h5>
+            <div className="row justify-content-md-center">
+                <div className="col-md-auto">
+                    <h5 id="info-id">My Info:
+                    <a href="https://bit.ly/3eKK7uy" target="_blank" rel='noreferrer noopener'><i className="bi bi-github"></i></a> 
+                    <a href="https://bit.ly/3x0CDd4" target="_blank" rel='noreferrer noopener'><i className="bi bi-linkedin"></i></a> 
+                    <a href="https://bit.ly/3fbDD8b" target="_blank" rel='noreferrer noopener'><i class="bi bi-stack"></i></a>
+                    </h5>
+                </div>
+            </div>
+            <div className="row justify-content-md-center">
+                <div className="col-md-auto">
+                    <h5 datatest-id="date">Copyright &copy; {new Date().getFullYear()}</h5>
+                </div>
             </div>
 
         </footer>
