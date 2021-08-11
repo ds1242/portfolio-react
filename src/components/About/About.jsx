c5f91b04bb389f7815558109f4b741dd00a37ac3
ds1242 <david.shaw1242@gmail.com>
2021-08-11 07:32:40 -0600

    added icons to about me section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import coverImage from '../../assets/imgs/mountain2-min.jpg';
 import awesomeImage from '../../assets/imgs/IMG_3529-min.jpg';
+import { Icon } from '@iconify/react';
 import './About.css';
 
 
@@ -23,11 +24,30 @@ function About(props) {
                     </div>
                 </div>
             </div>  
-            <div className="container-fluid">
+
                 <div className="row" id="about-title">
                     <h2 id="about">Skills</h2>
+                    <div className='row justify-content-center'>
+                        <div className='col col-lg-12 '>
+                            <Icon icon="logos:html-5" id='icons'/>
+                            <Icon icon="logos:css-3" id='icons'/>
+                            <Icon icon="logos:javascript" id='icons'/>
+                            <Icon icon="logos:jquery" id='icons'/>
+                            <Icon icon="logos:react" id='icons'/>
+                            <Icon icon="logos:handlebars" id='icons'/>
+                            <Icon icon="logos:mongodb" id='icons'/>
+                            <Icon icon="logos:mysql" id='icons'/>
+                            <Icon icon="logos:sequelize" id='icons'/>
+                            <Icon icon="logos:graphql" id='icons'/>
+                            <Icon icon="logos:nodejs" id='icons'/>
+                            <Icon icon="logos:express" id='icons'/>
+                            <Icon icon="logos:github-icon" id='icons'/>
+                            <Icon icon="logos:bootstrap" id='icons'/>                        
+                            <Icon icon="logos:npm" id='icons'/>
+                        </div>
+                    </div>
                 </div>
-            </div>  
+ 
         </section>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 // import Modal from 'react-bootstrap/Modal';
 // import Button from 'react-bootstrap/Button';
 // import $ from 'jquery';
 // import Popper from 'popper.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './components/App';
 import "bootstrap-icons/font/bootstrap-icons.css";
+
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
