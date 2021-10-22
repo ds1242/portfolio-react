fa149243aabcf99c5540ac2bf3e7ec0f411a1fad
ds1242 <david.shaw1242@gmail.com>
2021-10-22 17:10:59 -0600

    updated test snapshots for components

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Nav from '..';
+import Nav from '../Nav';
 
 
 afterEach(cleanup);
